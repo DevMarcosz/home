1bc670bdcfde68655f5dab62bba695ac929242dd
Marcos Douglas <marcosdouglas321@gmail.com>
2020-09-06 02:55:39 -0300

    Updates

---
+++ precache-manifest.11ecacf0e490dd05a8de1630744c8d22.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faef85ab535c66cfe2b8f0a0197a24cf",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "28a20575d0c5204f0265",
+    "url": "/home/static/css/main.e9084563.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/home/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/home/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28a20575d0c5204f0265",
+    "url": "/home/static/js/main.978252ef.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "70978561aaf60470b633c220b09f41ad",
+    "url": "/home/static/media/resume.70978561.pdf"
+  }
+]);
